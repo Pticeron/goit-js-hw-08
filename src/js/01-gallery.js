d9dc699a745da4b38885dfb32fbff9b69610854d
Inna Zorina <innazorina0101@gmail.com>
2023-03-02 21:01:05 +0200

    виконує рефакторинг галереї

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute('title', element.description);
+  galleryImage.alt = element.description;
+
+  galleryLink.append(galleryImage);
+  items.push(galleryLink);
+});
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
