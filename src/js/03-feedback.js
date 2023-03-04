012ce284e3ae7fa1af51f9751068e86a127b8f6e
Inna Zorina <innazorina0101@gmail.com>
2023-03-04 14:35:44 +0200

    додає функції для роботи зі storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,7 @@
+import throttle from "lodash.throttle";
 const STORAGE_KEY = `feedback-form-state`;
 
+const formData = {};
 const refs = {
     form: document.querySelector(`.feedback-form`),
     textarea: document.querySelector(`.feedback-form textarea`),
@@ -8,24 +10,30 @@ const refs = {
 populatTextarea();
 
 refs.form.addEventListener(`submit`, onFormSubmit);
-refs.textarea.addEventListener(`input`, onTextareaInput);
+refs.textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
 
-function onFormSubmit(evt) {
-    evt.preventDefault();
-    
-    evt.currentTarget.reset();
+refs.form.addEventListener(`input`, (e) => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
 
-    localStorage.removeItem(STORAGE_KEY);
-
-}
-
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+})
 
 function onTextareaInput(evt) {
     const message = evt.target.value;
-   
-    localStorage.setItem(STORAGE_KEY, message)
+
+    localStorage.setItem(STORAGE_KEY, message);
 }
 
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(`Submit form`);
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+}
 
 function populatTextarea() {
     const saveMessage = localStorage.getItem(STORAGE_KEY);
@@ -36,3 +44,4 @@ function populatTextarea() {
 
     refs.textarea.value = saveMessage;
 }
+
---
+++ src/js/storage.js
@@ -0,0 +1,21 @@
+//* функції для роботи зі storage
+
+const save = (key, value) => {
+  try {
+    const serializedData = JSON.stringify(value);
+    localStorage.setItem(key, serializedData);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedData = localStorage.getItem(key);
+    return serializedData === null ? undefined : JSON.parse(serializedData);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+export { save, load };
