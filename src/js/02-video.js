9603818b72c883874aec13a9822acb2c5a3121c3
Inna Zorina <innazorina0101@gmail.com>
2023-03-04 15:51:07 +0200

    додає Vimeo плеєр

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from "@vimeo/player";
+
+import { save, load } from "./storage";
+import throttle from 'lodash.throttle';
+
+// initialization of the player
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+function onTimeupdate({ seconds }) {
+  save(STORAGE_KEY, seconds);
+}
+const durationTime = load(STORAGE_KEY);
+player.setCurrentTime(durationTime || 0);
+
+
+// timeupdate {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+// }
+
 
