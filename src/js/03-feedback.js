14aca0045aad530e8d3e6e2df583fe2d04d52688
Inna Zorina <innazorina0101@gmail.com>
2023-03-04 15:16:16 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,60 @@
+import { save, load } from "./storage"; //фунцкії для роботи з Localstorage
 import throttle from "lodash.throttle";
+
 const STORAGE_KEY = `feedback-form-state`;
 
-const formData = {};
+let formData = load(STORAGE_KEY) || {};
+
 const refs = {
     form: document.querySelector(`.feedback-form`),
     textarea: document.querySelector(`.feedback-form textarea`),
+    input: document.querySelector(`.feedback-form input`),
 }
 
-populatTextarea();
+refs.input.setAttribute('required', '');
+refs.textarea.setAttribute('required', '');
 
 refs.form.addEventListener(`submit`, onFormSubmit);
-refs.textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
+refs.form.addEventListener(`input`, throttle(onFormInput, 500));
 
-refs.form.addEventListener(`input`, (e) => {
-    // console.log(e.target.name);
-    // console.log(e.target.value);
+populatFormData();
 
-    formData[e.target.name] = e.target.value;
-    console.log(formData);
-})
-
-function onTextareaInput(evt) {
-    const message = evt.target.value;
-
-    localStorage.setItem(STORAGE_KEY, message);
-}
-
-
-function onFormSubmit(evt) {
-    evt.preventDefault();
-    console.log(`Submit form`);
-    evt.currentTarget.reset();
-
-    localStorage.removeItem(STORAGE_KEY);
-}
-
-function populatTextarea() {
-    const saveMessage = localStorage.getItem(STORAGE_KEY);
-    
-    if (saveMessage) {
-        console.log(saveMessage);
+function populatFormData() {
+    if (formData !== undefined) {
+        refs.input.value = formData.email || "";
+        refs.textarea.value = formData.message || "";
     }
 
-    refs.textarea.value = saveMessage;
+    // refs.textarea.value = saveMessage;
 }
+
+
+// refs.form.addEventListener(`input`, (e) => {
+//     // console.log(e.target.name);
+//     // console.log(e.target.value);
+
+//     formData[e.target.name] = e.target.value;
+//     console.log(formData);
+// })
+
+// function onTextareaInput(evt) {
+//     const message = evt.target.value;
+
+//     localStorage.setItem(STORAGE_KEY, message);
+// }
+
+function onFormInput( { target } ) {
+    formData[target.name] = target.value.trim();
+    save(STORAGE_KEY, formData);
+}
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+  console.log(formData);
+  refs.form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
 
