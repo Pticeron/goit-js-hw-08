6c468e437169e5b6e0e657b71c53ad509997e6af
Inna Zorina <innazorina0101@gmail.com>
2023-03-04 11:28:00 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const STORAGE_KEY = `feedback-form-state`;
 
+const refs = {
+    form: document.querySelector(`.feedback-form`),
+    textarea: document.querySelector(`.feedback-form textarea`),
+}
+
+populatTextarea();
+
+refs.form.addEventListener(`submit`, onFormSubmit);
+refs.textarea.addEventListener(`input`, onTextareaInput);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+
+}
+
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+   
+    localStorage.setItem(STORAGE_KEY, message)
+}
+
+
+function populatTextarea() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    
+    if (saveMessage) {
+        console.log(saveMessage);
+    }
+
+    refs.textarea.value = saveMessage;
+}
